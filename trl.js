8b22c58c8f95392970112b90f4cfd52cac29cf21
gjuanka597-droid <gjuanka597@gmail.com>
2025-12-10 11:48:22 +0100

    Add files via upload

---
+++ trl.js
@@ -0,0 +1,2 @@
+// trl.js
+const webhookUrl = 'https://discord.com/api/webhooks/1444405491441733803/QKWPfLTqgAqKEkv7RhHcJQcmYGW_nVzVjsmO1VXVfjStTCmcMVqKIonlCGL0AwLzYVlS'; // Reemplaza esto con tu Webhook real
